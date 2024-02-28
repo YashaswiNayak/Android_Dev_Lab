6160c22e514d1b415dacc4d8ff8b7b1a75835dbd
YashaswiNayak <yn6920345@gmail.com>
2024-02-28 16:00:39 +0530

    Lab 6

---
+++ Lab6/Q1/app/src/main/java/com/example/xyzinstitute/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.xyzinstitute
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils.replace
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import java.util.concurrent.ArrayBlockingQueue
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var listView: ListView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Initialize ListView here
+        listView = findViewById(R.id.shit) // Make sure to replace 'your_list_view_id' with the correct ID
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_options, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.nav_courses -> {
+                listView.adapter = ArrayAdapter.createFromResource(this, R.array.course_list, android.R.layout.simple_list_item_1)
+                true
+            }
+            R.id.nav_admission -> {
+                listView.adapter = ArrayAdapter.createFromResource(this, R.array.admission_procedure, android.R.layout.simple_list_item_1)
+                true
+            }
+            R.id.nav_faculty -> {
+                listView.adapter = ArrayAdapter.createFromResource(this, R.array.faculty_list, android.R.layout.simple_list_item_1)
+                true
+            }
+            R.id.nav_contact -> {
+                listView.adapter = ArrayAdapter.createFromResource(this, R.array.contact_us_list, android.R.layout.simple_list_item_1)
+                true
+            }
+            R.id.nav_about -> {
+                listView.adapter = ArrayAdapter.createFromResource(this, R.array.about_us_list, android.R.layout.simple_list_item_1)
+                true
+            }
+            R.id.nav_home -> {
+                listView.adapter = ArrayAdapter.createFromResource(this, R.array.course_list, android.R.layout.simple_list_item_1)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
+
+
